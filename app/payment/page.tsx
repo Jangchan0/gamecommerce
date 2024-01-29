3488b5840e663b5462fab0db542eb530500638c9
jangchanyoung <jchy16@gmail.com>
2024-01-29 20:54:08 +0900

    FEAT: 로그인 필요 페이지 -> 로그인페이지 이동 구현

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,6 +1,22 @@
+'use client';
 import React from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { app } from '../firebase';
 
 const Payment = () => {
+    const router = useRouter();
+    React.useEffect(() => {
+        const auth = getAuth(app);
+
+        const loginVerification = onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                alert('로그인이 필요합니다');
+                router.push('/auth/login');
+            }
+        });
+        return () => loginVerification();
+    }, [router]);
     return <div>Payment</div>;
 };
 
