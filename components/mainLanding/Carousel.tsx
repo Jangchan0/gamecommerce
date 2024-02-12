7c6da528f4b315ff5379b19e2c5863f960d7d4b8
jangchanyoung <jchy16@gmail.com>
2024-02-12 23:40:05 +0900

    wip

--- components/mainLanding/Carousel.tsx
+++ components/mainLanding/Carousel.tsx
@@ -18,24 +18,12 @@ export default class Carousel extends Component {
         return (
             <div className="carousel">
                 <Slider {...this.settings}>
-                    <div>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </Slider>
             </div>
         );
