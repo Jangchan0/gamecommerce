0c7761d4e6c2a9ec7a854594a80bf165d7d55dff
jangchanyoung <jchy16@gmail.com>
2024-02-17 18:34:33 +0900

    MOIDFY: nav 메뉴 수정(기획변경)

--- components/nav/page.tsx
+++ components/nav/page.tsx
@@ -3,44 +3,34 @@ import Link from 'next/link';
 
 const Nav = () => {
     const officialGame = [
-        { title: '오늘의 영상', link: '/today' },
-        { title: 'Best video', link: '/best' },
-        { title: 'New video', link: '/new' },
-        { title: 'With Friend', link: '/friends' },
-        { title: '장르별', link: '/genres' },
-    ];
-    const developmentGame = [
-        { title: 'video request', link: '/request' },
-        { title: 'Need To Feedback', link: '/needtofeedback' },
+        { title: '액션', link: '/list/액션' },
+        { title: '어드벤처', link: '/list/어드벤처' },
+        { title: '캐주얼', link: '/list/캐주얼' },
+        { title: '인디', link: '/list/인디' },
+        { title: '대규모', link: '/list/대규모' },
+        { title: '멀티 플레이어', link: '/list/멀티플레이어' },
+        { title: '레이싱', link: '/list/레이싱' },
+        { title: '시뮬레이션', link: '/list/시뮬레이션' },
+        { title: 'RPG', link: '/list/RPG' },
+        { title: '스포츠', link: '/list/스포츠' },
+        { title: '전략', link: '/list/전략' },
+        { title: '기타', link: '/list/기타' },
     ];
 
     return (
         <>
-            <div className="bg-slate-500 sticky left-0 top-0 h-[100vh] w-[250px] px-5 py-2 flex flex-col rounded-r-sm">
-                <h1 className="title text-4xl mb-11">Geagul</h1>
+            <div className="bg-slate-500 sticky left-0 top-0 h-[100vh] w-[250px] px-5 py-2 flex flex-col justify-between rounded-r-sm">
                 <div className="officialGame flex flex-col mb-10">
+                    <h1 className="title text-4xl mb-11">Geagul</h1>
                     <h2 className="text-xl mb-3 ">Category</h2>
                     <div className="pl-7 pt-2 ">
                         {officialGame.map((cate, i) => (
                             <Link key={i} href={cate.link}>
-                                <h3 className="mb-3 text-lg">{cate.title}</h3>
+                                <h3 className="mb-3 text-lg cursor-pointer">{cate.title}</h3>
                             </Link>
                         ))}
                     </div>
                 </div>
-
-                <div className="developmentGame flex flex-col mb-10">
-                    <h2 className="text-xl mb-3 ">의뢰 & 피드백</h2>
-                    <div className="pl-7 pt-2 ">
-                        {developmentGame.map((cate, i) => (
-                            <Link key={i} href={cate.link}>
-                                <h3 className="mb-3 text-lg">{cate.title}</h3>
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-
-                <div className="downloadedGames text-lg mb-10">다운받은 게임</div>
 
                 <div className="navUserInfo flex flex-col mt-4">
                     <div className="flex justify-between mb-6">
