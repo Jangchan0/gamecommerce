62e5cdf88e81098bc65014ad4069a07c05d79d1c
jangchanyoung <jchy16@gmail.com>
2024-02-18 22:45:40 +0900

    FEAT: 장바구니 컴포넌트 분리

--- app/shoppingCart/page.tsx
+++ app/shoppingCart/page.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import UseAuthVerification from '../../Hooks/UseAuthVerification';
+import CartItem from '../../components/shoppingCart/CartItem';
 
 export default function ShoppingCart() {
     UseAuthVerification();
@@ -20,39 +21,7 @@ export default function ShoppingCart() {
                         <th></th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <input checked="" id="item1" onChange={undefined} type="checkbox" />
-                        </td>
-                        <td>
-                            <div className="flex space-x-4 items-center">
-                                <img
-                                    alt="상품 이미지"
-                                    className="h-20 w-20 object-cover rounded-md"
-                                    height="80"
-                                    src="/placeholder.svg"
-                                    style={{
-                                        aspectRatio: '80/80',
-                                        objectFit: 'cover',
-                                    }}
-                                    width="80"
-                                />
-                                <div>
-                                    <p className="font-medium">피파 24</p>
-                                    <p className="text-sm text-gray-500">상품코드: 1</p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>1개</td>
-                        <td>100,000원</td>
-                        <td>100,000원</td>
-                        <td></td>
-                        <td>
-                            <button className="text-sm">삭제</button>
-                        </td>
-                    </tr>
-                </tbody>
+                <CartItem />
             </table>
             <div className="flex justify-between items-center mt-4">
                 <div>
