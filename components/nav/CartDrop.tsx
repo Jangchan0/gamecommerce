5170a945df3565943b3ae1042ed80f18945b19f4
jangchanyoung <jchy16@gmail.com>
2024-02-19 13:56:49 +0900

    FEAT: 장바구니 삭제 로직 추가

--- components/nav/CartDrop.tsx
+++ components/nav/CartDrop.tsx
@@ -4,8 +4,14 @@ import { useRecoilState } from 'recoil';
 import { cartState, isDropCart } from '../../recoil/atoms/recoilAtoms';
 
 const CartDrawer = () => {
-    const [cart] = useRecoilState(cartState);
+    const [cart, setCart] = useRecoilState(cartState);
     const [isDropOn, setIsDropOn] = useRecoilState(isDropCart);
+
+    const handleDelete = (index) => {
+        const updatedCart = [...cart];
+        updatedCart.splice(index, 1);
+        setCart(updatedCart);
+    };
 
     return (
         <div className={`absolute top-0 right--40 w-[250px] h-[400px] overflow-scroll bg-slate-400`}>
@@ -13,19 +19,19 @@ const CartDrawer = () => {
                 <h2>장바구니</h2>
                 <button onClick={() => setIsDropOn(!isDropOn)}>닫기</button>
             </div>
-            <div className="cart-items flex">
+            <div className="cart-items flex flex-col">
                 {cart.map((item, index) => (
-                    <>
-                        <div key={index} className="cart-item bg-slate-300 w-[80%] flex-col">
+                    <div key={index} className=" flex">
+                        <div className="cart-item bg-slate-300 w-[80%] flex-col">
                             <p>{item.name}</p>
                             <p>수량: {item.quantity}</p>
                             <p>총 가격: ${item.price * item.quantity}</p>
                         </div>
                         <div className="ml-auto flex flex-col items-center gap-2">
                             <button>Edit</button>
-                            <button>Delete</button>
+                            <button onClick={handleDelete}>Delete</button>
                         </div>
-                    </>
+                    </div>
                 ))}
             </div>
         </div>
